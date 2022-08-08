ce0b2df41460a98fd390e8147dd4a2f84653ff75
Taimoor <taimoor.m89@gmail.com>
2022-08-08 00:12:26 -0400

    added dynamic section headings

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const sectionHeading = document.querySelector("#sectionText");
+
+const headingChanger = (el) => {
+  const observer = new IntersectionObserver(
+    function (entries) {
+      if (entries[0].isIntersecting === true) {
+        sectionHeading.textContent = el.dataset.heading;
+        sectionHeading.style.animationName = "fadeIn";
+        sectionHeading.style.animationDuration = "2s";
+      } else {
+        sectionHeading.style.animation = "none";
+      }
+    },
+    { threshold: [0.7] }
+  );
+
+  observer.observe(el);
+};
+
+headingChanger(document.querySelector("#home"));
+headingChanger(document.querySelector("#skills"));
+headingChanger(document.querySelector("#projects"));
+headingChanger(document.querySelector("#contact"));
